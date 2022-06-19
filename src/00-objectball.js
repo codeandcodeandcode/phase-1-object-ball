ac40e338df107e95c43a5d14db433136a8911db1
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-19 09:08:06 +0300

    Created object data

---
+++ src/00-objectball.js
@@ -0,0 +1,14 @@
+const gameObject =()=>{
+
+    let allan = {
+        "name": "Alan Anderson",
+        "number": 0,
+        "shoe": 16,
+        "points": 22,
+        "rebounds": 12,
+        "assists": 12,
+        "steals": 3,
+        "blocks": 1,
+        "slamDunks": 1
+    }
+}    
